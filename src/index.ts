dc58954d088344c074105e26cdf04dd9af744cb9
knaus94 <mihitovdima@gmail.com>
2023-02-28 14:07:53 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,90 @@
-import Strategy from './strategy';
+import { Strategy as OpenIDStrategy } from '@passport-next/passport-openid';
+import SteamWebAPI from 'steamapi';
 
-// Expose Strategy.
-export default Strategy;
+interface Profile {
+    provider: string;
+}
 
-// Exports.
-export { Strategy };
+type ValidateCallback = (
+    req,
+    identifier: string,
+    profile: Profile,
+    done: (error: Error | null, user?: Profile, info?: { message: string }) => void,
+) => void;
+
+interface SteamOpenIDOptions {
+    providerURL: string;
+    stateless?: boolean;
+    apiKey: string;
+    profile: boolean;
+}
+
+function getUserProfile(
+    key: string,
+    steamID: string,
+    callback: (err?: Error | null, profile?: Profile) => void,
+) {
+    const steam = new SteamWebAPI(key);
+
+    steam
+        .getUserSummary(steamID)
+        .then((result) => {
+            callback(null, {
+                provider: 'steam',
+                ...result,
+            });
+        })
+        .catch((err) => {
+            callback(err);
+        });
+}
+
+export default class Strategy extends OpenIDStrategy {
+    public name: string;
+    public stateless: boolean;
+
+    constructor(options: SteamOpenIDOptions, validate: ValidateCallback) {
+        options.providerURL = options.providerURL || 'https://steamcommunity.com/openid';
+        options.stateless = options.stateless ?? true;
+
+        async function verify(
+            req,
+            identifier: string,
+            profile: Profile,
+            done: (error: Error | null, user?: Profile, info?: { message: string }) => void,
+        ) {
+            const validOpEndpoint = 'https://steamcommunity.com/openid/login';
+            const identifierRegex = /^https?:\/\/steamcommunity\.com\/openid\/id\/(\d+)$/;
+            try {
+                if (
+                    req.query['openid.op_endpoint'] !== validOpEndpoint ||
+                    !identifierRegex.test(identifier)
+                ) {
+                    return done(null, undefined, { message: 'Claimed identity is invalid.' });
+                }
+
+                const steamID = identifierRegex.exec(identifier)[1];
+
+                if (options.profile) {
+                    getUserProfile(options.apiKey, steamID, (err, profile) => {
+                        if (err) {
+                            done(err);
+                        } else {
+                            validate(req, identifier, profile, done);
+                        }
+                    });
+                } else {
+                    validate(req, identifier, profile, done);
+                }
+            } catch (err) {
+                done(err);
+            }
+        }
+
+        super(options, verify);
+        OpenIDStrategy.call(this, options, verify);
+
+        this.name = 'steam';
+        this.stateless = options.stateless;
+    }
+}
