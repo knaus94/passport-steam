131eef41c4af70d099aab9b9a04b7249da67a394
knaus94 <mihitovdima@gmail.com>
2023-02-28 13:20:49 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -20,41 +20,70 @@ interface SteamOpenIDOptions {
     profile: boolean;
 }
 
+function getUserProfile(
+    key: string,
+    steamID: string,
+    callback: (err?: Error | null, profile?: Profile) => void,
+) {
+    const steam = new SteamWebAPI(key);
+
+    steam
+        .getUserSummary(steamID)
+        .then((result) => {
+            callback(null, {
+                provider: 'steam',
+                ...result,
+            });
+        })
+        .catch((err) => {
+            callback(err);
+        });
+}
+
 export default class Strategy extends OpenIDStrategy {
-    public name = 'steam';
+    public name: string;
+    public stateless: boolean;
 
     constructor(options: SteamOpenIDOptions, validate: ValidateCallback) {
         options.providerURL = options.providerURL || 'https://steamcommunity.com/openid';
         options.stateless = options.stateless ?? true;
 
-        super(options, async (req: Request, identifier: string, profile: Profile, done) => {
+        async function verify(
+            req: Request,
+            identifier: string,
+            profile: Profile,
+            done: (error: Error | null, user?: Profile, info?: { message: string }) => void,
+        ) {
             const validOpEndpoint = 'https://steamcommunity.com/openid/login';
             const identifierRegex = /^https?:\/\/steamcommunity\.com\/openid\/id\/(\d+)$/;
 
-            if (
-                req.query['openid.op_endpoint'] !== validOpEndpoint ||
-                !identifierRegex.test(identifier)
-            ) {
-                return done(null, undefined, { message: 'Claimed identity is invalid.' });
-            }
-
-            const steamID = identifierRegex.exec(identifier)?.[1];
-
-            if (options.profile) {
-                const steam = new SteamWebAPI(options.apiKey);
-                try {
-                    const result = await steam.getUserSummary(steamID);
-                    const userProfile: Profile = {
-                        provider: this.name,
-                        ...result,
-                    };
-                    validate(req, identifier, userProfile, done);
-                } catch (err) {
-                    done(err);
+            try {
+                const opEndpoint = req.query['openid.op_endpoint'];
+                if (opEndpoint !== validOpEndpoint || !identifierRegex.test(identifier)) {
+                    return done(null, undefined, { message: 'Claimed identity is invalid.' });
                 }
-            } else {
-                validate(req, identifier, profile, done);
+
+                const steamID = identifierRegex.exec(identifier)[1];
+
+                if (options.profile) {
+                    getUserProfile(options.apiKey, steamID, (err, profile) => {
+                        if (err) {
+                            done(err);
+                        } else {
+                            validate(req, identifier, profile, done);
+                        }
+                    });
+                } else {
+                    validate(req, identifier, profile, done);
+                }
+            } catch (err) {
+                done(err);
             }
-        });
+        }
+
+        super(options, verify);
+
+        this.name = 'steam';
+        this.stateless = options.stateless;
     }
 }
